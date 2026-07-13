2ec0ca824aefbc58ddefc1c9f97f26ee5bd5e271
Rahul Sharma <hubstackrealm@gmail.com>
2026-07-13 23:03:36 +0530

    Update app.js
    1. Adding employee 2
    2. Adding Bonus method to class employee

--- app.js
+++ app.js
@@ -32,11 +32,31 @@ class Employee
     // using getters to get yearly salary
     get yearlySal()
     {
-        return this.annualSalary();
+        return "Annual Salary: " + this.annualSalary();
+    }
+
+    // calculating bonus
+    bonus()
+    {
+        // creating bonuses for specific departments
+        if(this.dept == "Frontend Web Developer" || this.dept == "AI Engineer")
+        {
+            return "Salary After 10% Bonus: " + (this.#salary + (this.#salary*0.1));
+        }
+        return "No Bonus for this dept. " + this.#salary;
     }
 
 }
 
-let employee1 = new Employee(001, "Rahul", "Sharma", "Frontend Web Developer", 70000, 5);
-console.log(employee1.yearlySal);
+let employee1 = new Employee(1001, "Rahul", "Sharma", "Frontend Web Developer", 70000, 5);
+let employee2 = new Employee(1002, "Guriqbal", "Singh", "SEO Specialist", 50000, 3);
+
+// Employee1
 console.log(employee1.employeeDetail());
+console.log(employee1.yearlySal);
+console.log(employee1.bonus());
+
+// Employee2
+console.log(employee2.employeeDetail());
+console.log(employee2.yearlySal);
+console.log(employee2.bonus());
