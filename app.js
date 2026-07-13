4b6438f0ad646525b611c2ad55812b99328ccd69
Rahul Sharma <hubstackrealm@gmail.com>
2026-07-13 22:50:52 +0530

    Update app.js
    Adding the
    1. Employee Details Method in the class.
    2. Employee Annual Salary.
    3. Getters to get the Annual Salary.

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 
+class Employee
+{
+
+    // making basic salary and years of experience private
+    #salary;
+    #yrexperience;
+
+    // an employee using constructor(special function) to allocate the fields to itself
+    constructor(empid, fname, lname, dept, salary, yrexperience)
+    {
+        this.empid = empid;
+        this.fname = fname;
+        this.lname = lname;
+        this.dept = dept;
+        this.#salary = salary;
+        this.#yrexperience = yrexperience;
+    }
+
+    // employee details method
+    employeeDetail()
+    {
+        return "Employee Id: " + this.empid + " . Employee Name: " + this.fname + " " + this.lname + " . Employee Department: " + this.dept + " . Employee Salary: " + this.#salary + " . Employee Years of Experience: " + this.#yrexperience;
+    }
+
+    // annual salary
+    annualSalary()
+    {
+        return (12*this.#salary);
+    }
+
+    // using getters to get yearly salary
+    get yearlySal()
+    {
+        return this.annualSalary();
+    }
+
+}
+
+let employee1 = new Employee(001, "Rahul", "Sharma", "Frontend Web Developer", 70000, 5);
+console.log(employee1.yearlySal);
+console.log(employee1.employeeDetail());
